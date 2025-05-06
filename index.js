5b28f92a564e126ba9f5d42209cbcf6757870e61
GadianoC <cgadiano22a-0029@olopsc.edu.ph>
2025-05-06 23:21:29 +0800

    changes lmao

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-    res.render('index', {title: 'Homepage', name: 'Guest'});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); 
 });
 
-app.listen(port, function(req, res) {
-    console.log(`Server Running at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
